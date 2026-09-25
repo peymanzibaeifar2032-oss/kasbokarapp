dd04d3e190b5cb85e4e9b8ecfbf307f2f8270bf0
peymanzibaeifar2032-oss <peyman.zibaeifar2032@gmail.com>
2026-09-25 06:54:39 +0300

    Restore tattoo-flow.ts

--- src/lib/tattoo-flow.ts
+++ src/lib/tattoo-flow.ts
@@ -1,1 +1,12 @@
-PLACEHOLDER
+export function makeTattooTrackingCode() {
+  return String(100000 + Math.floor(Math.random() * 900000));
+}
+
+export function normalizeTattooTrackingCode(value: string) {
+  const fold = value
+    .trim()
+    .replace(/[۰-۹]/g, (d) => String("۰۱۲۳۴۵۶۷۸۹".indexOf(d)))
+    .replace(/[٠-٩]/g, (d) => String("٠١٢٣٤٥٦٧٨٩".indexOf(d)))
+    .replace(/\D/g, "");
+  return fold;
+}
